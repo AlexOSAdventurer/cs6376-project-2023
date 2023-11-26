685a29338803f251c21ef7f664e657b1739771b9
Alex Richardson <alexrichardsonschool@gmail.com>
2023-11-26 17:30:44 -0600

    Removed unnecessary import.

--- train.py
+++ train.py
@@ -7,7 +7,6 @@ from time import strftime
 from copy import deepcopy
 
 from flow.core.util import ensure_dir
-from flow.utils.registry import env_constructor
 from flow.utils.rllib import FlowParamsEncoder, get_flow_params
 from flow.utils.registry import make_create_env
 
